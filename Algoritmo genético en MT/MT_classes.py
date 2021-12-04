45f2cf956bf1427a6210ad52965b5d0c807cd2d5
Ignacio Pita <napita2007@gmail.com>
2021-12-03 22:27:29 -0300

    multicinta

--- Algoritmo genético en MT/MT_classes.py
+++ Algoritmo genético en MT/MT_classes.py
@@ -1,6 +1,18 @@
+
+class cinta():
+    def __init__(self) -> None:
+        posicion = 0
+        lista = []
+
+
+
+
+
 class maquinaDeTuring():
     def __init__(self):
-        self.cinta = []
+        self.cinta = cinta
+        self.cintaFitness = cinta
+        self.cintaCruce = cinta
         self.posicion = 0
         self.estado = 0
         self.memoria = 0
@@ -14,7 +26,7 @@ class maquinaDeTuring():
         return self.cinta[self.posicion]
     
     def escribirCinta(self,valor):
-        self.cinta[self.posicion] = valor
+        self.cinta.lista[self.cinta.posicion] = valor
 
     def ejecutar(self):
         while self.estado != 'F':
@@ -48,7 +60,12 @@ class maquinaDeTuring():
         
   
     #anatomia de estados de transición: [estado, dirección, valor, nuevo estado]
-    #cinta ejemplo: [P,n,n,n,n,n,F]
+    #cinta ejemplo: 
+    """ [0,20,35,10,.5,0] <-Almacena el Fitness de cada individuo
+        [0,0,1,0,0,0] <-Almacena el estado de cada individuo
+        [P,15,100,60,90,F]<-Almacena símbolos de la cinta y valores de los individuos
+
+         """
 
 
     #estados de transición:[q0,0]=[0,R,q1]
@@ -121,83 +138,4 @@ class individuo:
         self.state = 0
     
 
-
-
-
-
-
-"""
-class Tape(object):
-    
-    blank_symbol = " "
-    
-    def __init__(self,
-                 tape_string = ""):
-        self.__tape = dict((enumerate(tape_string)))
-        # last line is equivalent to the following three lines:
-        #self.__tape = {}
-        #for i in range(len(tape_string)):
-        #    self.__tape[i] = input[i]
-        
-    def __str__(self):
-        s = ""
-        min_used_index = min(self.__tape.keys()) 
-        max_used_index = max(self.__tape.keys())
-        for i in range(min_used_index, max_used_index):
-            s += self.__tape[i]
-        return s    
-   
-    def __getitem__(self,index):
-        if index in self.__tape:
-            return self.__tape[index]
-        else:
-            return Tape.blank_symbol
-
-    def __setitem__(self, pos, char):
-        self.__tape[pos] = char 
-
-        
-class TuringMachine(object):
-    
-    def __init__(self, 
-                 tape = "", 
-                 blank_symbol = " ",
-                 initial_state = "",
-                 final_states = None,
-                 transition_function = None):
-        self.__tape = Tape(tape)
-        self.__head_position = 0
-        self.__blank_symbol = blank_symbol
-        self.__current_state = initial_state
-        if transition_function == None:
-            self.__transition_function = {}
-        else:
-            self.__transition_function = transition_function
-        if final_states == None:
-            self.__final_states = set()
-        else:
-            self.__final_states = set(final_states)
-        
-    def get_tape(self): 
-        return str(self.__tape)
-    
-    def step(self):
-        char_under_head = self.__tape[self.__head_position]
-        x = (self.__current_state, char_under_head)
-        if x in self.__transition_function:
-            y = self.__transition_function[x]
-            self.__tape[self.__head_position] = y[1]
-            if y[2] == "R":
-                self.__head_position += 1
-            elif y[2] == "L":
-                self.__head_position -= 1
-            self.__current_state = y[0]
-
-    def final(self):
-        if self.__current_state in self.__final_states:
-            return True
-        else:
-            return False
-
-"""
 
